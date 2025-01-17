f4c002a3b7c78aad87aff84e78d8a0fc0b95a1e1
ThainaSantoss <thainados_santos@hotmail.com>
2025-01-17 01:29:30 -0300

    adding list in reducer

---
+++ src/reducers/listReducer.ts
@@ -0,0 +1,64 @@
+import { Item } from "@/types/Item";
+
+type addAction = {
+  type: "add";
+  payload: {
+    text: string;
+  };
+};
+
+type EditTextAction = {
+  type: "editAction";
+  payload: {
+    id: number;
+    newText: string;
+  };
+};
+
+type ToggleDoneAction = {
+  type: "toggleDone";
+  payload: {
+    id: number;
+  };
+};
+
+type RemoveAction = {
+  type: "remove";
+  payload: {
+    id: number;
+  };
+};
+
+type ListAction = addAction | EditTextAction | ToggleDoneAction | RemoveAction;
+
+export const listReducer = (list: Item[], action: ListAction) => {
+  switch (action.type) {
+    case "add":
+        return [...list, {
+            id: list.length,
+            text: action.payload.text ,
+            done: false
+        }]
+    case "editAction":
+       return list.map( t => {
+            if(t.id === action.payload.id) {
+                t.text = action.payload.newText
+            }
+            return t;
+        });
+
+    case "toggleDone":
+          return list.map( t => {
+                if(t.id === action.payload.id) t.done = !t.done
+                return t;
+            });
+    case "remove":
+        return list.filter(t => t.id !== action.payload.id);
+    default:
+        return list;
+  }
+};
+
+// executar alguma ação: recebe,executa e retorna
+// qual ação executada
+// carga que vai ser mandada
